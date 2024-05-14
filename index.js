2ac77c04f0b6de818b802d3d70ea76e79b42369c
Lava Ahmed <lavaahmed915@gmail.com>
2024-05-14 18:59:40 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = word.split("").reverse();
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== reversedWord[i]) {
+      return false;
+    } else{
+      return true;
+    }
+  }
 }
 
 /* 
   Add your pseudocode here
+  first i reverse the word then check each letter in the both words
+  if they are the same then return true else return false
+
 */
 
 /*
   Add written explanation of your solution here
+  make a variable for reversed word and make this word as array then reverse it,
+  the loop over word and reversed word if they are the same then return true
 */
 
 // You can run `node index.js` to view these console logs
